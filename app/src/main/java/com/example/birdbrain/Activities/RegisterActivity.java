a86eacadef8a9d317a398be6e6ec3ced1714540f
Alexander Hunter <alexjhunter0@gmail.com>
2024-04-22 14:56:30 -0400

    Registration working.

--- app/src/main/java/com/example/birdbrain/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/birdbrain/Activities/RegisterActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +12,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.birdbrain.Database.Repository;
 import com.example.birdbrain.R;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -18,11 +20,14 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText editTextUsername;
     private EditText editTextPassword;
     private Button buttonRegister;
+    private Repository Repository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Repository = new Repository(getApplication());
 
         editTextUsername = findViewById(R.id.editTextUsername);
         editTextPassword = findViewById(R.id.editTextPassword);
@@ -31,13 +36,20 @@ public class RegisterActivity extends AppCompatActivity {
         buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = editTextUsername.getText().toString().trim();
-                String password = editTextPassword.getText().toString().trim();
-
-                // Here, you should add your logic to register the user, e.g., adding them to a database
-                // For now, just finish the activity to return to MainActivity
+                registerUser();
                 finish();
             }
         });
+    }
+
+    private void registerUser() {
+        String username = editTextUsername.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (!username.isEmpty() && !password.isEmpty()) {
+            Repository.registerUser(username, password);
+        } else {
+            Toast.makeText(this, "Username or password cannot be empty", Toast.LENGTH_LONG).show();
+        }
     }
 }
